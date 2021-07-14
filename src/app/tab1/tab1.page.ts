1bfb74ba11ffc165addc2629832d2938c4739b52
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-07-14 12:32:45 -0700

    added changes to home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,24 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public concerts = [
+  public featuredEvents = [
     {
-      name: "concert1",
-      date: "day/month",
+      name: "Dodgers game",
+      date: "6/21",
       img: "../../assets/dodgers.jpeg"
-    },
+    }, 
     {
-      name: "concert1",
-      date: "day/month",
-      img: "../../assets/dodgers.jpeg"
-    },
-    {
-      name: "concert1",
-      date: "day/month",
-      img: "../../assets/dodgers.jpeg"
+      name: "Bad Bunny concert",
+      date: "7/4",
+      img: "../../assets/concert.1.jpeg"
+    },{
+      name: "OC Fair",
+      date: "7/6",
+      img: "../../assets/ocFair.jpeg"
     }
   ]
-  public sportsEvents = []
+
+public sportsEvents = [
+  {
+    name: "concert1",
+    date: "day/month",
+    img: "../../assets/coachella.jpeg"
+  }
+]
+slideOpts = {
+  slidesPerView: 3
+}
   constructor() {}
   
 
