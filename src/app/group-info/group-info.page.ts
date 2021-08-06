122b6de7588c34732f6e76110bfaa333471419e8
TXT-39 <txt-39@MacBook-Pro-42.attlocal.net>
2021-08-05 21:12:16 -0700

    added a more detailed group info page

--- src/app/group-info/group-info.page.ts
+++ src/app/group-info/group-info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-group-info',
@@ -6,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-info.page.scss'],
 })
 export class GroupInfoPage implements OnInit {
+  slideOpts = {
+    slidesPerView: 1.1,
+  }
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Request Sent!',
+      buttons: ['OK'],
+    });
 
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
   ngOnInit() {
   }
 
